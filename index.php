7e4db670be6a48f8c1f05e4deb72594a9badd508
gretanorv <gretanorv@gmail.com>
2020-12-28 15:05:39 +0200

    code refactor

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,96 +17,91 @@
     <h1 class="title">File explorer</h1>
 
     <?php
-
-    //on load initiate
     if (!isset($_GET['dir'])) {
         $curr_dir = '..';
-        $home = scanHere($curr_dir);
-        createTable($home, $curr_dir);
-    } else if (isset($_GET['dir'])) {
-        $new_dir = $_GET['dir'];
-        $test = scanHere($new_dir);
-        createTable($test, $new_dir);
+    } else {
+        $curr_dir = $_GET['dir'];
     }
 
-
-    function scanHere($dir)
-    {
-        $a = scandir($dir);
-        return $a;
-    }
-
-    function createTable($dir, $path)
-    {
-        print('<div class="container">');
-
-
-        //BACK button
-        if (isset($_GET['dir']) and $_GET['dir'] != '..') {
-            $_SERVER['REQUEST_URI'] = preg_replace('#\/[^/]*$#', '$1', $_SERVER['REQUEST_URI']) . "\n";;
-            print('<a class="table__nav" href=' . $_SERVER['REQUEST_URI'] . '>Back</a>');
+    if (isset($_POST['newFolder']) and $_POST['newFolder'] != '') {
+        if (!file_exists($curr_dir . '/' . $_POST['newFolder'])) {
+            mkdir($curr_dir . '/' . $_POST['newFolder']);
+            array_push(scandir($curr_dir), $_POST['newFolder']);
+        } else {
+            //TODO:: place message nicely
+            print('Folder name exists');
         }
+    } elseif ($_POST['newFolder'] === '') {
+        //TODO:: place message nicely
+        print('Folder name is empty');
+    }
 
+    //DELETE button
+    if ($_GET['action'] && $_GET['action'] == 'delete') {
+        unlink($_GET['filename']);
+        header("Location:index.php");
+        exit();
+    }
+    ?>
 
-        //CREATE NEW button
-        print('
-            <form action="" method="post" class="form">
-                <input type="text" name="newFolder" id="newFolder" class="form__input" placeholder="Folder name">
-                <input type="submit" value="Create" class="form__button">
-            </form>
-        ');
-        if (isset($_POST['newFolder']) and $_POST['newFolder'] != '') {
-            if (!file_exists($path . '/' . $_POST['newFolder'])) {
-                mkdir($path . '/' . $_POST['newFolder']);
-                array_push($dir, $_POST['newFolder']);
-            } else {
-                print('Folder name exists');
+    <div class="container">
+        <form action="" method="post" class="form">
+            <input type="text" name="newFolder" id="newFolder" class="form__input" placeholder="Folder name">
+            <input type="submit" value="Create" class="form__button">
+        </form>
+        <div class="table">
+            <?php
+            //BACK button logic
+            if (isset($_GET['dir']) and $_GET['dir'] != '..') {
+                $_SERVER['REQUEST_URI'] = preg_replace('#\/[^/]*$#', '$1', $_SERVER['REQUEST_URI']) . "\n";;
+                print('<a class="table__nav" href=' . $_SERVER['REQUEST_URI'] . '>Back</a>');
             }
-        } elseif ($_POST['newFolder'] === '') {
-            print('Folder name is empty');
-        }
 
+            createTable($curr_dir);
+            ?>
+        </div>
+    </div>
 
-        //DELETE button
-        if ($_GET['action'] && $_GET['action'] == 'delete') {
-            unlink($_GET['filename']);
-            header("Location:index.php");
-            exit();
-        }
+    <?php
+    function createTable($path)
+    {
+        $dir = scandir($path);
 
-        print('<div class="table">');
-
-        //table
         $i = 2;
         for ($i; $i < count($dir); $i++) {
-
             if (is_dir("{$path}/{$dir[$i]}")) {
-                print('<div class="table__row">');
-                print('<div class="table__row-left">Directory</div>');
-                print("<div class='table__row-right'>
-                            <a 
-                                class='table__row-link' 
-                                href='index.php?dir={$path}/{$dir[$i]}'
-                            >{$dir[$i]}
-                            </a>
-                        </div>");
-                print('</div>');
-            } else if (is_file("{$path}/{$dir[$i]}")) {
-                print('<div class="table__row">');
-                print("<div class='table__row-left'>File</div>");
-                print("<div class='table__row-right'>
-                    {$dir[$i]}
-                    <a href='index.php?action=delete&filename={$path}/{$dir[$i]}' class='table__row-right-btn'>
-                        DELETE
-                    </a>
-                    </div>");
-                print('</div>');
+                $folder = true;
+                $href_dir = "index.php?dir={$path}/{$dir[$i]}";
+                $type = 'Directory';
+                $file_name = $dir[$i];
+            } elseif (is_file("{$path}/{$dir[$i]}")) {
+                $folder = false;
+                //TODO:: if ending .php then no delete action
+                $del_btn = true;
+                $href_dir = "index.php?action=delete&filename={$path}/{$dir[$i]}";
+                $type = 'File';
+                $file_name = $dir[$i];
             }
+    ?>
+            <div class="table__row">
+                <div class="table__row-left"><?php echo $type ?></div>
+                <div class='table__row-right'>
+                    <?php
+                    if ($folder) {
+                        print("<a class='table__row-link' href='{$href_dir}'>{$file_name}
+                        </a>");
+                    } else {
+                        print("{$file_name}");
+                        if ($del_btn) {
+                            print("<a href='{$href_dir}' class='table__row-right-btn'>DELETE</a>");
+                        }
+                    }
+                    ?>
+                </div>
+            </div>
+    <?php
         }
-        print('</div>');
-        print('</div>');
     }
-
     ?>
 
     <script> </script>
